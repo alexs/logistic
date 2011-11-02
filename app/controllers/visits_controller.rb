c86db8c58b92e2a191135a0a02d8fa8284937fc5
Alex Silva <alexs1010@gmail.com>
2011-11-02 02:38:25 -0600

    finish with 4 report

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -28,6 +28,12 @@ class VisitsController < ApplicationController
       send_file "#{::Rails.root.to_s}/public/excel/#{title}", :type => 'application/vnd.ms-excel', :filename => title
   end
 
+  def download_report4
+      title = "instituciones_visitadas.xls"
+      GenerateSheet.report4
+      send_file "#{::Rails.root.to_s}/public/excel/#{title}", :type => 'application/vnd.ms-excel', :filename => title
+  end
+
 
   # GET /visits/1
   # GET /visits/1.xml
--- lib/generate_sheet.rb
+++ lib/generate_sheet.rb
@@ -69,4 +69,24 @@ class GenerateSheet
 
     book.write ::Rails.root.to_s + '/public/excel/instituciones_con_solicitud.xls'
     end
+
+    def self.report4
+    Spreadsheet.client_encoding = 'UTF-8'
+      book = Spreadsheet.open ::Rails.root.to_s + '/public/excel/Reporte4_machote.xls'
+    sheet = book.worksheet 0
+
+    i = 12
+      UserInstitution.find_all_by_status(2).each do |solicitud|
+        @total_registros_institucion = UserInstitution.find_all_by_institution_id(solicitud.institution_id)
+
+        sheet[i,0] = solicitud.institution.name
+        @total_registros_institucion.each do |registro|
+          sheet[i,1] = registro.user.full_name
+          sheet[i,2] = registro.desc
+          i = i + 1
+        end
+      end
+
+    book.write ::Rails.root.to_s + '/public/excel/instituciones_visitadas.xls'
+    end
   end
