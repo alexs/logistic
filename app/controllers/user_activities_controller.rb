d766f45c15dba36bf861a0c8b0cbd078d424da6c
Alex Silva <alexs1010@gmail.com>
2011-11-12 10:29:22 -0600

    add new system of select activites

--- app/controllers/user_activities_controller.rb
+++ app/controllers/user_activities_controller.rb
@@ -34,13 +34,9 @@ class UserActivitiesController < ApplicationController
     @activities = params[:activity]
 
     @activities.each_value do |activity|
-      @date = '2011-11-' + params[:date][activity.to_s]
-
-      @hora_inicio = params[:start_hour][activity.to_s]+":"+params[:start_minute][activity.to_s]
-      @hora_fin = params[:end_hour][activity.to_s]+":"+params[:end_minute][activity.to_s]
       @desc = params[:activity_desc][activity.to_s]
 
-      @user_activity = UserActivity.new(:user_id => current_user.id, :activity_id => activity, :date  => "#{@date}", :start_hour => "#{Time.parse(@hora_inicio)}", :end_hour => "#{Time.parse(@hora_fin)}", :desc => @desc, :status => 1)
+      @user_activity = UserActivity.new(:user_id => current_user.id, :activity_id => activity, :desc => @desc, :status => 1)
       if @user_activity.save
       else
       end
--- app/models/user_activity.rb
+++ app/models/user_activity.rb
@@ -1,4 +1,6 @@
 class UserActivity < ActiveRecord::Base
   belongs_to :activity
   belongs_to :user
+
+ validates_uniqueness_of :activity_id, :scope => [:user_id]
 end
